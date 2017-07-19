48b850ccffddffed1cf7ef89f13fe1d3b65d03a6
David <wizzardx@gmail.com>
2017-07-19 19:02:00 +0200

    Revisions to 07_array_of_n_elements

--- 005_hackerrank_challenges/001_introduction/007_array_of_n_elements/README.txt
+++ 005_hackerrank_challenges/001_introduction/007_array_of_n_elements/README.txt
@@ -1,1 +1,3 @@
 https://www.hackerrank.com/challenges/fp-array-of-n-elements
+
+Run ./test.sh to run the logic, run lints, etc.
--- 005_hackerrank_challenges/001_introduction/007_array_of_n_elements/solution.hs
+++ 005_hackerrank_challenges/001_introduction/007_array_of_n_elements/solution.hs
@@ -1,1 +1,5 @@
-fn n = [1..n]
+fn :: Integer -> [Integer]
+fn n = [1::Integer .. n]
+
+main :: IO ()
+main = print $ fn 5  -- 5 is just any random sample number
