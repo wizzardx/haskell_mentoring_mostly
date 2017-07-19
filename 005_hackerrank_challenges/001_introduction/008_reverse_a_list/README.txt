9d986513c7d6cbbef10132371e64576b295899c6
David <wizzardx@gmail.com>
2017-07-19 19:44:07 +0200

    Some revisions to 008_reverse_a_list

--- 005_hackerrank_challenges/001_introduction/008_reverse_a_list/README.txt
+++ 005_hackerrank_challenges/001_introduction/008_reverse_a_list/README.txt
@@ -1,1 +1,3 @@
 https://www.hackerrank.com/challenges/fp-reverse-a-list
+
+Run ./test.sh to run the logic, run lints, etc.
--- 005_hackerrank_challenges/001_introduction/008_reverse_a_list/solution.hs
+++ 005_hackerrank_challenges/001_introduction/008_reverse_a_list/solution.hs
@@ -1,4 +1,19 @@
-rev l = go l
-    where go [] = []
-          go [x] = [x]
-          go (x:xs) = (go xs) ++ [x]
+rev :: [a] -> [a]
+rev [] = []
+rev [x] = [x]
+rev (x:xs) = rev xs ++ [x]
+
+-- Update: Handré made some really good commentary on how the above is pretty
+-- inefficient for longer lists, and gave some other good solutions (that I'm
+-- unable to fully understand at the moment)
+--
+-- Over here I'll just leave a link to how the Haskell Prelude defines reverse:
+--    http://hackage.haskell.org/package/base-4.9.1.0/docs/src/GHC.List.html#reverse
+--
+
+-- Just so that things can run (we're not using the sample provided from the
+-- exercise page, since they don't provide code for reading stdin and stdout,
+-- and I can't yet write or understand that code without copypasting from
+-- other examples:
+main :: IO ()
+main = print $ rev [1::Int, 2, 3, 4, 5]
